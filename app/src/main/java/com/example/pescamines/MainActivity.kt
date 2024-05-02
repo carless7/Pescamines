8a33800a8db48b9766c091939ee0f73b48064865
oscarvdcr <73657935+oscarvdcr@users.noreply.github.com>
2024-05-02 18:16:09 +0200

    Commit: Simple implementation of main menu

--- app/src/main/java/com/example/pescamines/MainActivity.kt
+++ app/src/main/java/com/example/pescamines/MainActivity.kt
@@ -1,46 +1,77 @@
 package com.example.pescamines
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.pescamines.ui.theme.PescaminesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PescaminesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                //color = background(colorResource(id = R.color.my_color))
+            ) {
+                Minesweeper()
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PescaminesTheme {
-        Greeting("Android")
+fun Minesweeper() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MainScreen(navController) }
+        composable("help") { HelpScreen() }
+        composable("config") { ConfigurationScreen() }
     }
 }
+@Composable
+fun MainScreen(navController: NavController) {
+    val activity = (LocalContext.current as? Activity)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Button(onClick = { navController.navigate("help") }) {
+            Text("Com jugar")
+        }
+        Button(onClick = { navController.navigate("config") }) {
+            Text("Nova partida")
+        }
+        Button(onClick = { activity?.finish() }) {
+            Text("Sortir")
+        }
+    }
+}
+@Composable
+fun HelpScreen() {
+    Text("Pàgina d'ajuda")
+}
+@Composable
+fun ConfigurationScreen() {
+    Text("Pàgina de configuració del joc")
+}
+
